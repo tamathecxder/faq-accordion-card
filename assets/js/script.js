7b47139b21b270195ec2ea78b974dd6ebfcf5468
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-06-26 10:40:02 +0700

    Menyelesaikan styling responsive all device dan menuliskan terlebih dahulu script untuk accordion FAQ

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Accordion
+const accordion = document.getElementsByClassName('accordion') ;
+var i;
+
+for ( i = 0; i < accordion.length; i++ ) {
+  accordion[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+
+    const panel = this.nextElementSibling;
+    if ( panel.style.maxHeight ) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
